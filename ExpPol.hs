3d59783d6f32c621661dd347d0327d076b8799ef
FabrZ <22534021+FabriZM@users.noreply.github.com>
2025-11-27 17:22:35 -0300

    Implementar maxProd, gradoEP, calcEP, sumList y prodList

--- ExpPol.hs
+++ ExpPol.hs
@@ -24,34 +24,64 @@ cantPol = \t -> case t of {
 	Pol p -> 1;
 	Der d -> cantPol d;
 	Eval a n -> 1;
-	Sum y -> sum (map cantPol y);
+	Sum x -> sum (map cantPol x);
 	Prod x -> sum (map cantPol x); 
 }
 
 --11)
 cantx :: ExPol -> Int
 cantx  = \t -> case t of {
-	Pol p -> length (filter (snd > 0) p);
+	Pol p -> length (filter (> 0) (map snd p));
 	Der d -> cantx d;
 	Eval a n -> cantx a;
-	Sum y -> sum (map cantx y);
+	Sum x -> sum (map cantx x);
 	Prod x -> sum (map cantx x); 
 }
 
 --12)
 maxProd :: ExPol -> Int
-maxProd  = undefined
+maxProd  = \t -> case t of {
+	Pol p -> 0;
+	Der d -> maxProd d;
+	Eval a n -> 0;
+	Sum x -> maximum (map maxProd x);
+	Prod x -> maximum [length x, maximum (map maxProd x)]; 
+}
 
 
 --13)
 gradoEP :: ExPol -> Int
-gradoEP = undefined 
+gradoEP = \t -> case t of {
+	Pol p -> maximum (map snd p);
+	Der d -> gradoEP d;
+	Eval a n -> gradoEP a;
+	Sum x -> maximum (map gradoEP x);
+	Prod x -> maximum (map gradoEP x);
+}  
 	
---14)	
+-- 14)	
 calcEP :: ExPol -> Polinomio
-calcEP = undefined  
+calcEP = \t -> case t of {
+    Pol p -> redPol p;
+    Der d -> redPol (derPol (calcEP d));
+    Eval a n -> redPol ([(evalPol (calcEP a) n, 0)]);
+    Sum x -> sumList x;
+    Prod x -> prodList x;
+}
+
+sumList :: [ExPol] -> Polinomio
+sumList = \x -> case x of {
+    [] -> [];
+    y:ys -> redPol (sumPol (calcEP y) (sumList ys));
+}
+
+prodList :: [ExPol] -> Polinomio
+prodList = \x -> case x of {
+    [] -> [(1,0)];
+    y:ys -> redPol (mulPol (calcEP y) (prodList ys));
+}
 
 --15)
 resultado :: ExPol -> String
-resultado = undefined
+resultado = \t -> showPol (calcEP t);
 
